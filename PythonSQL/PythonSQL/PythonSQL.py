04c8cc0a51a3a4f70ecb7a77c54bd6e63d999d7f
Alex <alex.grig.sur@gmail.com>
2020-12-21 07:04:45 +0300

    sql

--- PythonSQL/PythonSQL/PythonSQL.py
+++ PythonSQL/PythonSQL/PythonSQL.py
@@ -16,28 +16,30 @@ def executeReader(str):
 def AddCity(city):
     executeCommand(f"INSERT INTO Cities VALUES (null,'{city}')")
 
-def addBuildAddr(city_ID,building_name, address):
-    executeCommand(f"INSERT INTO buildings_addr VALUES (null,'{city_ID}', '{building_name}', '{address}')")
+def addBuilding(city_ID,building_name, address):
+    executeCommand(f"INSERT INTO buildings VALUES (null,'{city_ID}', '{building_name}', '{address}')")
 
-def addBuilding(building_addr_id, cab, floor):
-    executeCommand(f"INSERT INTO buildings VALUES ('{building_addr_id}', '{cab}', '{floor}')")
+def addCabinet(building_id, cab, floor):
+    executeCommand(f"INSERT INTO cabinets VALUES ('{building_id}', '{cab}', '{floor}')")
 
 
-def printsCities(fio):
+def printsCities():
     rows = executeReader(f"SELECT city FROM cities")
     for row in rows:
         print(row)
     return len(rows)
 
 
-def printBuildingsAddr():
-    rows = executeReader(f"SELECT cities.city, building_name FROM buildings_addr JOIN cities ON buildings_addr.city_id = cities.id")
+def printCabinets():
+    #("SELECT * FROM buildings_addr")
+    rows = executeReader("SELECT cities.city from buildings join cities on cities.id=buildings.city_id")#("SELECT city, building_name, building_address FROM buildings JOIN cities ON buildings.city_id = cities.id")
     for row in rows:
         print(row)
     return len(rows)
 
-def printBuildings():
-    rows = executeReader(f"SELECT buildings_addr.building_name, building_cab, building_floor FROM buildings JOIN buildings_addr ON buildings.building_addr_id = buildings_addr.id")
+def printCabinets():
+    #("SELECT * FROM buildings")
+    rows = executeReader("SELECT buildings.building_name, cabinets.cabinet_cab, cabinets.cabinet_floor FROM cabinets JOIN buildings ON cabinets.building_id = buildings.id")
     for row in rows:
         print(row)
     return len(rows)
@@ -47,7 +49,6 @@ def printtable(tablename) -> int:
     for row in rows:
         print(row)
     return len(rows)
-
 
 def gettable(tablename) -> list:
     rows = executeReader(f"SELECT * FROM {tablename}")
@@ -63,35 +64,46 @@ def initializationTables():
     cursor.execute("""CREATE TABLE IF NOT EXISTS cities(
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, 
     city text)""")
-    cursor.execute("""CREATE TABLE IF NOT EXISTS buildings_addr(
+    cursor.execute("""CREATE TABLE IF NOT EXISTS buildings(
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     city_id INTEGER NOT NULL,
     building_name text,
     building_address text,
     FOREIGN KEY (city_ID) REFERENCES cities(id))""")
-    cursor.execute("""CREATE TABLE IF NOT EXISTS buildings(
-    building_addr_id INTEGER NOT NULL, 
-    building_cab INTEGER NOT NULL, 
-    building_floor INTEGER NOT NULL, 
-    FOREIGN KEY (building_addr_id) REFERENCES buildings_addr(id),
-    CONSTRAINT pr_key PRIMARY KEY (building_addr_id,building_cab))""")
+    cursor.execute("""CREATE TABLE IF NOT EXISTS cabinets(
+    building_id INTEGER NOT NULL, 
+    cabinet_cab INTEGER NOT NULL, 
+    cabinet_floor INTEGER NOT NULL, 
+    FOREIGN KEY (building_id) REFERENCES buildings(id),
+    CONSTRAINT pr_key PRIMARY KEY (building_id,cabinet_cab))""")
     connection.commit()
 
+def DropAll():
+    executeCommand("Drop table cities")
+    executeCommand("Drop table buildings")
+    executeCommand("Drop table cabinets")
+
+
 def ClearAll():
+    executeCommand("Delete from cabinets")
     executeCommand("Delete from buildings")
-    executeCommand("Delete from buildings_addr")
     executeCommand("Delete from cities")
 
 if __name__ == '__main__':
     connection = sqlite3.connect('buildings.db')
     cursor = connection.cursor()
 
+    DropAll()
     initializationTables()
-    ClearAll()
+    #ClearAll()
 
     AddCity('Краснодар')
-    addBuildAddr(0,"КубГУ","Ставропольская 149")
-    addBuilding(0,100,0)
-    addBuilding(0,200,0)
+    addBuilding(0,"КубГУ","Ставропольская 149")
+    addCabinet(0,100,0)
+    addCabinet(0,200,0)
 
-    printtable("buildings")
+    printsCities()
+    print("________________________")
+    printCabinets()
+    print("________________________")
+    printCabinets()
